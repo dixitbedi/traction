15ddeff42df0ea86a76ccfbca347f2864e1c6cb6
Dixit Bedi <dixit.bedi@gmail.com>
2021-01-08 13:06:30 +0530

    Updated driver

--- conf.js
+++ conf.js
@@ -38,7 +38,7 @@ exports.config = {
         {
             'browserName': 'chrome',
             'chromeOptions': {
-                'args': ['--disable-extensions','disable-infobars','--disable-dev-shm-usage']
+                'args': ['--no-sandbox','--disable-extensions','disable-infobars','--disable-dev-shm-usage']
             }
         },
         // {
